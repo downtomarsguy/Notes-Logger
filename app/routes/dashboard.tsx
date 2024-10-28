912d24ef20562a92b6fcee1569684d9938ce8410
theonesilverest <theonesilverest@gmail.com>
2024-10-28 11:58:05 +0000

    Dashboard Frontend Updated; Title, Add File and Search Added

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,14 +1,10 @@
 // existing imports
-import {
-  Form,
-  Link,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-} from "@remix-run/react";
+import { useNavigate, useLoaderData } from "@remix-run/react";
+
+// import icons
+import { FaRegSquarePlus } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 
 // supabase configuration
 import { createServerClient } from '@supabase/auth-helpers-remix';
@@ -33,7 +29,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   );
 };
 
-// meta configuration
+// meta configuration 
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
@@ -43,10 +39,37 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+interface Title {
+  title: string;
+}
+
 export default function App() {
+  const navigate = useNavigate();
+  const { data, supabaseUrl, supabaseKey } = useLoaderData<{ data: Title[], supabaseUrl: string, supabaseKey: string }>();
+
   return (
-    <div className="font-sans">
-      hello
+    <div className="flex justify-center">
+      <div className="font-sans w-3/4 mt-16">
+        <p className="font-arima font-bold text-4xl">Dashboard</p>
+        <div className="flex flex-row mt-2 gap-[10px]">
+          <button className="flex items-center bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 px-3 py-2 rounded-[3px]">
+            <span className="mr-2 font-sans text-sm font-bold">Add File</span>
+            <FaPlus />
+          </button>
+          <div className="flex flex-row gap-[5px]">
+            <input
+              type="text"
+              name="passcode"
+              placeholder="Search Files.."
+              className="bg-white bg-opacity-20 px-3 py-2 w-[360px] text-sm font-bold rounded-[3px] font-sans"
+            />
+            <button className="flex items-center bg-white bg-opacity-40 hover:bg-opacity-50 active:bg-opacity-60 px-3 py-2 rounded-[3px]">
+              <FaSearch size={14} />
+            </button>
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
